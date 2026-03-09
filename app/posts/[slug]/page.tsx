635fa4d0e5295d4ddec2f8fef5218b75126d9e6f
EHOIL728 <ty000728@g.hongik.ac.kr>
2026-03-09 11:45:57 +0900

    bug fix

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function PostPage({
 }) {
   const { slug } = await params;
 
-  const filePath = path.join(process.cwd(), "content", "${slug}.mdx");
+  const filePath = path.join(process.cwd(), "content", `${slug}.mdx`);
   const fileContent = fs.readFileSync(filePath, "utf8");
 
   const { content, data } = matter(fileContent);
